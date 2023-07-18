13ca04bfefc910fa4ee73c6aa49248dc4b056d55
Shreyanshi200 <120807907+Shreyanshi200@users.noreply.github.com>
2023-07-18 11:37:15 +0530

    first commit

---
+++ src/socket.js
@@ -0,0 +1,13 @@
+import { io } from "socket.io-client";
+
+export const initSocket = async () => {
+  // console.log('socket init');
+  const options = {
+    "force new connection": true,
+    reconnectionAttempt: Infinity,
+    timeout: 10000,
+    transports: ["websocket"],
+  };
+  
+  return io(process.env.REACT_APP_BACKEND_URL, options);
+};
